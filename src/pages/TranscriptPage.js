37f12c145cb48f2d2ed8540b28e72ad882ee1057
Maxmerock123 <Maxmerock@gmail.com>
2023-11-22 12:25:07 +0700

    add bootstrap css framework
    also add card components in TranscriptPage

--- src/pages/TranscriptPage.js
+++ src/pages/TranscriptPage.js
@@ -7,12 +7,48 @@ import '@fontsource/roboto/700.css';
 const TranscriptPage = () => {
   return (
     <>
-    <h1>Activity Transcript</h1>
-    <div>
-        <h3>กิจกรรม</h3>
-        <h3>กิจกรรม</h3>
-        <h3>กิจกรรม</h3>
-    </div>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></link>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <h2 class="navbar-brand" href="#">NisitKU</h2>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Activity Transcript</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Registration Planner</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <h1 className='center'>Activity Transcript</h1>
+      <div>
+        <div class="card m-5">
+          <div class="card m-2">
+            <div className='card-body'>
+              activity 1
+            </div>
+          </div>  
+          <div class="card m-2">
+            <div className='card-body'>
+              activity 1
+            </div>
+          </div>  
+          <div class="card m-2">
+            <div className='card-body'>
+              activity 1
+            </div>
+          </div>  
+        </div>
+      </div>
     </>
   )
 }
