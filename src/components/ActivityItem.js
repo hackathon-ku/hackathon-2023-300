6ae9ed9692139b821bea8388c591e4bd7203cc03
Maxmerock123 <Maxmerock@gmail.com>
2023-11-23 09:25:27 +0700

    add google font to index.css

--- src/components/ActivityItem.js
+++ src/components/ActivityItem.js
@@ -3,6 +3,10 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 import '../index.css';
+import "@fontsource/istok-web"; // Defaults to weight 400
+import "@fontsource/istok-web/400.css"; // Specify weight
+import "@fontsource/istok-web/400-italic.css"; // Specify weight and style
+
 
 const ActivityItem = () => {
   const [activityData, setActivityData] = useState([]);
@@ -17,22 +21,24 @@ const ActivityItem = () => {
 
   return (
     <>
-      {activityData.map((item) => (
-        <Card className='m-5 card-user'>
-          <div className='img-container'>
-            <button className='img-mockup'>Image here</button>
-          </div>
-          <Card.Body className=''>
-            <Card.Title>{item.Title}</Card.Title>
-            <Card.Text>
-              {item.body}
-            </Card.Text>
-            <Button variant="primary" >read more</Button>
-          </Card.Body>
-        </Card>
-      ))}
-    </>
-  );
+        {activityData.map((item) => (
+          <Card className='m-5 card-user'>
+            <div className='img-container'>
+              <button className='img-mockup'>Image here</button>
+            </div>
+            <Card.Body className=''>
+              <Card.Title>{item.Title}</Card.Title>
+              <Card.Text>
+                {item.body}
+              </Card.Text>
+              <div className='d-flex justify-content-end'>
+                <Button variant="primary" >read more</Button>
+              </div>
+            </Card.Body>
+          </Card>
+        ))}
+      </>
+      );
 };
 
-export default ActivityItem;
+      export default ActivityItem;
