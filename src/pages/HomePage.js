7561b410459e8b0dd0bada7d355bfe4c94f4daf2
Maxmerock123 <Maxmerock@gmail.com>
2023-11-23 18:26:03 +0700

    fix display bug, add profile picture

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,25 +11,36 @@ const HomePage = () => {
                     <div className="row">
                         <div className="col-lg-4">
                             <div className="card mb-4">
-                                <div className="card-body text-center">
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
-                                        className="rounded-circle img-fluid" style={{ width: '150px' }} />
+                                <div className="card-body text-center align-">
+                                    <div className='d-flex justify-center '>
+                                        <img
+                                            src="https://i.postimg.cc/HLssP1Cw/student-profile.jpg"
+                                            alt="avatar"
+                                            className="img-fluid"
+                                            style={{
+                                                width: '100%', // Make sure the image covers the entire container
+                                                height: '100%',
+                                                objectFit: 'cover', // Crop the image
+                                                objectPosition: 'center center', // Center the image
+                                            }}
+                                        />
+                                    </div>
                                     <h5 className="my-3">John Smith</h5>
-                                    <p className="text-muted mb-1">คณะมนุษยศาสตร์ สาขานวัตกรรมการท่องเที่ยว </p>
+                                    <p className="text-muted mb-1">คณะวิศกรรมศาสตร์ สาขาไฟฟ้า </p>
                                     <p className="text-muted mb-4">ชั้นปีที่ 4</p>
                                     <div className="d-flex justify-content-center mb-2">
                                     </div>
                                 </div>
-                                
+
                             </div>
                         </div>
                         <label>Past Activity</label>
-                        <ActivityItem/>
                     </div>
+                        <ActivityItem />
                 </div>
-        </section >
-    </>
-  )
+            </section >
+        </>
+    )
 }
 
 export default HomePage
